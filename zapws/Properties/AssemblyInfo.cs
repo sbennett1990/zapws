27201bd6db81c293800a8b712ae4d60befad2fb7
Scott Bennett <scottb@fastmail.com>
2020-08-21 10:57:32 -0400

    Fill out a little more for the assembly

--- zapws/Properties/AssemblyInfo.cs
+++ zapws/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("zapws")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Display or remove trailing whitespace from a file.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("zapws")]
-[assembly: AssemblyCopyright("Copyright (c)  2019")]
+[assembly: AssemblyCopyright("Copyright (c)  2019, 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6")]
+[assembly: AssemblyFileVersion("0.6")]
