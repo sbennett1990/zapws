b85e9b062054046fe6cde657167416fb69624c89
Scott Bennett <scottb@fastmail.com>
2020-08-23 23:57:48 -0400

    First stabe at creating a generic template for C# programs

---
+++ mono.prog.mk
@@ -0,0 +1,94 @@
+# Template for building Mono C# applications on OpenBSD
+
+PROPS?=
+SRCDIR?=	src
+BINDIR?=	bin
+
+# Enumerate libraries used
+LIBS?=
+
+# Use major version 7.0 as a default
+LANG?=	7.0
+
+# Ensure required variables are set
+.poison empty (PROG)
+.poison empty (SRCS)
+.poison empty (MAIN)
+.poison empty (BINDIR)
+
+# Set compiler flags
+.ifmake debug
+OUT=	${BINDIR}/Debug/${PROG}
+.else
+OUT=	${BINDIR}/${PROG}
+.endif
+
+FLAGS=	-nologo -main:${MAIN} -out:${OUT}
+FLAGS+=	-langversion:\"${LANG}\"
+.if defined(APPCONFIG)
+FLAGS+=	-appconfig:${APPCONFIG}
+.endif
+# Add in libraries
+.if defined(LIBS)
+.  ifmake debug
+FLAGS+=	-lib:${BINDIR}/Debug
+.  else
+FLAGS+=	-lib:${BINDIR}
+.  endif
+.  for l in ${LIBS}
+FLAGS+=	-reference:${l}.dll
+.  endfor
+.endif
+DFLAGS=	-debug -define:DEBUG -optimize-
+
+# Real location of the C# source files
+CS=	${SRCS:S,^,$(SRCDIR)/,g}
+
+all release: testobj libs
+	csc ${FLAGS} ${CS} ${PROPS}
+	@echo '${PROG:R} -> ${.CURDIR}/${OUT}'
+
+debug: testobj libs
+	csc ${FLAGS} ${DFLAGS} ${CS} ${PROPS}
+	@echo '${PROG:R} -> ${.CURDIR}/${OUT}'
+
+doc: testobj
+.if defined(DOCFILE)
+	csc ${FLAGS} -doc:${DOCFILE} ${CS}
+	@echo '${PROG:R} -> ${.CURDIR}/${OUT}'
+.else
+	@echo 'No DOCFILE defined, nothing to do'
+.endif
+
+testobj:
+.if !exists(${BINDIR})
+	@echo 'No output directory! Run `make obj` before proceeding'
+	@false
+.endif
+
+obj:
+.if !exists(${BINDIR})
+	mkdir -p ${BINDIR}/Debug
+.endif
+
+libs:
+.if defined(LIBS)
+	@echo '===> building libs'
+.  ifmake debug
+	cd ${.CURDIR}/lib && make obj && make debug
+.    for l in ${LIBS}
+	@cp ${.CURDIR}/lib/bin/Debug/${l}{.dll,.pdb} ${BINDIR}/Debug
+.    endfor
+.  else
+	cd ${.CURDIR}/lib && make obj && exec make
+.    for l in ${LIBS}
+	@cp ${.CURDIR}/lib/bin/${l}.dll ${BINDIR}
+.    endfor
+.  endif
+	@echo '===> done with libs'
+.endif
+
+clean:
+	rm -rf ${BINDIR}
+	rm -f ${PROG} ${DOCFILE}
+	@mkdir -p ${BINDIR}/Debug
